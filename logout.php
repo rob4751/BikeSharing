926a9206d4010a31c16d49852a00391bab3fd19f
rob4751 <103455121+rob4751@users.noreply.github.com>
2025-05-25 04:31:37 +0200

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();     // Remove all session variables
-session_destroy();   // Destroy the session completely
-
-// Optional: redirect to login or home page
-header("Location: index.php");
-exit();
